a7e213aba8460dd62c93db98f623582dc589ccb2
Karl Moran <karlmoran06@gmail.com>
2022-08-09 12:21:31 +0000

    NavbarSimple challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,17 @@
 import css from './App.module.css';
 import Sidebar from './components/Sidebar';
+import StatefulGreeting from './components/StatefulGreeting';
+import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+import NavBarSimple from './components/NavbarSimple';
 
 function App() {
   return (
     <div className={css.App}>
       <Sidebar />
+      <br></br>
+      <StatefulGreeting greeting="I'm a stateful class component!"/>
+      <StatefulGreetingWithPrevState />
+      <NavBarSimple />
     </div>
   );
 }
