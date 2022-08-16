1e8aec5f1c5b55d9cfa8fb846e19783c3ee40f3b
Karl Moran <karlmoran06@gmail.com>
2022-08-16 12:52:06 +0000

    compenent LifeCycle

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,14 @@
 import css from './App.module.css';
+// import LifeCyclesCDU from './components/LifeCyclesCDU';
 // import Sidebar from './components/Sidebar';
 // import StatefulGreeting from './components/StatefulGreeting';
 // import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
 // import NavBarSimple from './components/NavbarSimple';
-import Content from './components/Content';
+// import Content from './components/Content';
+// import LifeCyclesCDM from "./components/LifeCyclesCDM";
+// import LifeCyclesCDU from "./components/LifeCyclesCDU";
+import LifeCyclesCWU from "./components/LifeCyclesCWU";
+
 
 function App() {
   return (
@@ -12,7 +17,11 @@ function App() {
       <StatefulGreeting greeting="I'm a stateful class component!"/>
       <StatefulGreetingWithPrevState />
       <NavBarSimple /> */}
-      <Content />
+      {/* <Content />
+      <Clock /> */}
+      {/* <LifeCyclesCDM /> */}
+      {/* <LifeCyclesCDU /> */}
+      <LifeCyclesCWU />
     </div>
   );
 }
