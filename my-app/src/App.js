8db9d4e0763e453c1c94a65b7dc9e5bed6b4f2bc
Karl Moran <karlmoran06@gmail.com>
2022-08-18 12:13:49 +0000

    Forms / Searchbar / search feature challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,10 +4,14 @@ import css from './App.module.css';
 // import StatefulGreeting from './components/StatefulGreeting';
 // import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
 // import NavBarSimple from './components/NavbarSimple';
-import Content from './components/Content';
+// import Content from './components/Content';
+// import ControlledForm from './components/ControlledForm';
 // import LifeCyclesCDM from "./components/LifeCyclesCDM";
 // import LifeCyclesCDU from "./components/LifeCyclesCDU";
 // import LifeCyclesCWU from "./components/LifeCyclesCWU";
+// import UncontrolledForm from "./components/UncontrolledForm";
+// import SearchBar from "./components/SearchBar";
+import ContentSearch from "./components/ContentSearch";
 
 
 function App() {
@@ -17,11 +21,15 @@ function App() {
       <StatefulGreeting greeting="I'm a stateful class component!"/>
       <StatefulGreetingWithPrevState />
       <NavBarSimple /> */}
-       <Content />
+       {/* <Content /> */}
        {/* <Clock />  */}
       {/* <LifeCyclesCDM /> */}
       {/* <LifeCyclesCDU /> */}
       {/* <LifeCyclesCWU /> */}
+      {/* <ControlledForm /> */}
+      {/* <UncontrolledForm /> */}
+      {/* <SearchBar /> */}
+      <ContentSearch />
     </div>
   );
 }
---
+++ my-app/src/components/ContentSearch.js
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+import css from "./css/Content.module.css";
+import {savedPosts} from "../posts.json";
+import PostItem from "./PostItem";
+import Loader from "./Loader";
+
+export class Content extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoaded: false,
+            posts: [],
+        }
+    }
+
+    componentDidMount() {
+        setTimeout(()=>{
+            this.setState({
+                isLoaded: true,
+                posts: savedPosts,
+            })
+        }, 2000)
+    }
+
+    handleChange = (e) => {
+        const name = e.target.value.toLowerCase();
+        const filteredPosts = savedPosts.filter((post)=>{
+            return post.name.toLowerCase().includes(name);
+        })
+        
+        this.setState({
+            posts: filteredPosts
+        })
+    }
+    
+    render() {
+        return (
+            <div className={css.Content}>
+                
+                <div className={css.TitleBar}>
+                    <h1>My Photos</h1>
+                    <form>
+                        <label htmlFor='searchinput'>Search</label>
+                        <input 
+                        type='search' 
+                        id='searchinput' 
+                        placeholder='By Author'
+                        onChange={(e) => this.handleChange(e)}
+                        />
+                        <h4>posts found {this.state.posts.length}</h4>
+                    </form>
+                </div>
+
+                <div className={css.SearchResults}>
+                    {
+                        this.state.isLoaded ?
+                        <PostItem savedPosts={savedPosts} />
+                        : <Loader />
+                    }
+
+                    {/* Part 1: Creating the map function */}
+
+                        {/* {
+                    savedPosts.map((post)=>{
+                            return <div className={css.SearchItem} key={post.title}>
+                                <p>{post.title}</p>
+                                <p>{post.name}</p>
+                                <img src={post.image} alt="random"/>
+                                <p>{post.description}</p>
+                                </div>
+                        })
+                    } */}
+
+
+                    {/* Part 2: Creating a child component */}
+
+                    <PostItem savedPosts={this.state.posts} />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Content
